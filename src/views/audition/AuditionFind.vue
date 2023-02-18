f14ce2eac9e73b83933af1819c24caf54c22c656
gringrape <gringrape200@gmail.com>
2023-02-18 19:31:41 +0900

    검색 필터 정상화
    각 검색 필터의 내용을 올바르게 작동하도록 수정하고,
    제작사 이름이나 영화 제목에 대해서도 검색이 되도록 수정함.

--- src/views/audition/AuditionFind.vue
+++ src/views/audition/AuditionFind.vue
@@ -10,6 +10,10 @@ import {
 import AuditionItem from '../../components/autidtion/AuditionItem.vue';
 import { isScrappedAudition } from '../../service/actors';
 
+function isOverlapped(range1, range2) {
+  return Math.min(range1[1], range2[1]) >= Math.max(range1[0], range2[0]);
+}
+
 export default {
   name: 'audition-find',
   setup() {
@@ -24,7 +28,7 @@ export default {
       sort: '1',
       kind: '1',
       heightRange: [130, 200],
-      MAP: [
+      SKILLS: [
         { id: 'A', value: '영어' },
         { id: 'B', value: '중국어' },
         { id: 'C', value: '일본어' },
@@ -51,7 +55,6 @@ export default {
         { text: 'OTT시리즈', id: 'ott' },
         { text: '웹시리즈', id: 'web' },
         { text: '광고', id: 'ad' },
-
       ], // 장르별
       curatedList: [
         { text: '성별', id: 'gender' },
@@ -65,7 +68,7 @@ export default {
       selectedHeightRange: null,
       selectedGender: null,
       selectedPrefer: [],
-
+      searchKeyword: '',
     };
   },
   methods: {
@@ -79,37 +82,73 @@ export default {
       audition.isScrap = !audition.isScrap;
     },
     handleClickGenreGroup(genre) {
-      if (genre.id === 'all') {
+      const { id: genreId } = genre;
+
+      if (genreId === 'all') {
         this.selectedGenreList = ['all'];
         return;
       }
+
       if (this.selectedGenreList.includes('all')) {
-        this.selectedGenreList = this.selectedGenreList.filter((v) => v !== 'all');
+        this.selectedGenreList = this.selectedGenreList
+          .filter((v) => v !== 'all');
       }
-      if (this.selectedGenreList.includes(genre.id)) {
-        this.selectedGenreList = this.selectedGenreList.filter((v) => v !== genre.id);
-      } else {
-        this.selectedGenreList.push(genre.id);
-      }
+
+      this.selectedGenreList = this.selectedGenreList.includes(genreId)
+        ? this.selectedGenreList.filter((v) => v !== genreId)
+        : [...this.selectedGenreList, genreId];
+    },
+    handleClickShowAll() {
+      this.searchClear();
     },
     handleClickSearchClear() {
+      this.searchClear();
+    },
+    searchClear() {
       this.selectedGenreList = ['all'];
       this.kind = '1';
       this.selectedAgeRange = null;
       this.selectedGender = null;
       this.selectedHeightRange = null;
       this.selectedPrefer = [];
+      this.searchKeyword = '';
     },
     handleClickOverlay() {
     },
-    handleAgeRangeChange(value) {
-      this.selectedAgeRange = value;
+    handleAgeRangeChange(range) {
+      this.selectedAgeRange = range;
     },
     handleHeightRangeChange(value) {
       this.selectedHeightRange = value;
     },
     handleClickGenderChange(value) {
       this.selectedGender = value;
+    },
+    handleSearchText() {
+      const auditions = JSON.parse(
+        JSON.stringify(this.$store.state.auditionList),
+      );
+
+      this.filteredAuditionList = this.applyFilters({
+        target: auditions,
+        filters: [
+        ],
+      });
+    },
+    applyFilters({ target, filters }) {
+      if (filters.length === 0) {
+        return target;
+      }
+
+      const { condition, filter } = filters[0];
+      if (!condition) {
+        return this.applyFilters({ target, filters: filters.slice(1) });
+      }
+
+      return this.applyFilters({
+        target: target.filter(filter),
+        filters: filters.slice(1),
+      });
     },
   },
   mounted() {
@@ -124,20 +163,46 @@ export default {
       return list;
     },
     filteredAuditionList() {
-      let list = JSON.parse(JSON.stringify(this.$store.state.auditionList));
-      if (this.selectedGender && this.selectedGender === 'male') {
-        list = list.filter((v) => v.gender === '1');
-      } else if (this.selectedGender && this.selectedGender === 'female') {
-        list = list.filter((v) => v.gender === '2');
-      }
-      if (this.selectedPrefer.length > 0) {
-        list = list.filter((v) => this.selectedPrefer.includes(v.prefer));
-      }
+      const auditions = JSON.parse(
+        JSON.stringify(this.$store.state.auditionList),
+      );
 
-      if (this.selectedAgeRange && this.selectedAgeRange.length > 1) {
-        console.log('list', list);
-      }
-      return list;
+      const filterSkill = (audition) => this.selectedPrefer.includes(audition.prefer);
+
+      const filterAge = (audition) => {
+        const selectedRange = Object.values(this.selectedAgeRange);
+        return isOverlapped(audition.ageRange, selectedRange);
+      };
+
+      const filterHeight = (audition) => {
+        const selectedHeightRange = Object.values(this.selectedHeightRange);
+        return isOverlapped(audition.heightRange, selectedHeightRange);
+      };
+
+      const filterGender = (audition) => {
+        const selectedGender = { male: '1', female: '2' }[this.selectedGender];
+        return audition.gender === selectedGender;
+      };
+
+      const filterGenres = (audition) => (
+        this.selectedGenreList.includes('all') || this.selectedGenreList.includes(audition.genre)
+      );
+
+      const filterKeyword = ({ title, productionName }) => (
+        title.includes(this.searchKeyword) || productionName.includes(this.searchKeyword)
+      );
+
+      return this.applyFilters({
+        target: auditions,
+        filters: [
+          { condition: this.selectedPrefer.length > 0, filter: filterSkill },
+          { condition: this.selectedGender, filter: filterGender },
+          { condition: this.selectedAgeRange, filter: filterAge },
+          { condition: this.selectedHeightRange, filter: filterHeight },
+          { condition: this.selectedGenreList.length > 0, filter: filterGenres },
+          { condition: this.searchKeyword, filter: filterKeyword },
+        ],
+      });
     },
   },
   components: {
@@ -150,7 +215,9 @@ export default {
     <div class="audition-find">
         <div class="audition-find__header">
             <a-radio-group v-model:value="kind" size="large">
-                <a-radio-button size="large" value="1">전체 오디션</a-radio-button>
+                <a-radio-button size="large" value="1" @click="handleClickShowAll">
+                  전체 오디션
+                </a-radio-button>
                 <a-radio-button value="2">장르별 보기
                     <DownOutlined />
                 </a-radio-button>
@@ -168,6 +235,8 @@ export default {
             <a-input
               class="item"
               size="large"
+              v-model:value="searchKeyword"
+              v-on:keyup.enter="handleSearchText"
               style="width:240px; margin-left:auto;" placeholder="검색"
             >
                 <template #addonBefore>
@@ -252,7 +321,7 @@ export default {
                                               @change="handleCheckboxChange"
                                             >
                                                 <a-row :gutter="[0, 20]">
-                                                    <template v-for="box in MAP" :key="box.id">
+                                                    <template v-for="box in SKILLS" :key="box.id">
                                                         <a-col :span="10">
                                                             <a-checkbox :value="box.id">
                                                               {{ box.value }}
