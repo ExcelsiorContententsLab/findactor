c1b995cecddf0857e77cefdd34993d97df338914
gringrape <gringrape200@gmail.com>
2023-02-23 10:53:23 +0900

    오디션 목록 인자 없는 경우 수정 :

--- src/service/auditions.js
+++ src/service/auditions.js
@@ -2,7 +2,9 @@ import axios from 'axios';
 
 const API_URL = 'https://findactor.shop';
 
-export async function loadAuditions({ productionName }) {
+export async function loadAuditions({ productionName } = {
+  productionName: '',
+}) {
   const { data } = await axios.get(`${API_URL}/auditions?productionName=${productionName}`);
   return data;
 }
